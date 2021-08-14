d416ca3d88c7148323590cf4f83627468da4c8e8
alexfj498 <alejfuejur@gmail.com>
2021-08-14 03:09:41 +0200

    New config variables

--- src/config.py
+++ src/config.py
@@ -1,3 +1,20 @@
 START = "startcap"
 STOP = "endcap"
 EMBEDDING_SIZE = 300
+
+"""VALUES FOR INCEPTION MODEL"""
+INCEPTION_WIDTH = 299
+INCEPTION_HEIGHT = 299
+INCEPTION_OUTPUT_DIM = 2048
+"""--------------------------"""
+
+"""VALUES FOR VGG16 MODEL"""
+VGG16_WIDTH = 224
+VGG16_HEIGHT = 224
+VGG16_OUTPUT_DIM = 4096
+"""--------------------------"""
+
+"""VALUES FOR RNN MODEL"""
+LOSS = "sparse_categorical_crossentropy"
+OPTIMIZER = "adam"
+METRICS = ["accuracy"]
